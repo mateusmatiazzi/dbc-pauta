59d6e8a45a5b82e3a1157f1136024901c582e564
mateusmatiazzi <mateuspaivamatiazzi1@gmail.com>
2021-10-24 11:14:53 -0300

    Refatoração no serviço inicio de votação

--- src/main/java/br/com/dbc/pauta/dbcpauta/http/PautaWS.java
+++ src/main/java/br/com/dbc/pauta/dbcpauta/http/PautaWS.java
@@ -1,9 +1,7 @@
 package br.com.dbc.pauta.dbcpauta.http;
 
 import br.com.dbc.pauta.dbcpauta.http.domain.request.PautaRequest;
-import br.com.dbc.pauta.dbcpauta.http.domain.request.SessaoVotacaoRequest;
 import br.com.dbc.pauta.dbcpauta.usecase.CadastrarPauta;
-import br.com.dbc.pauta.dbcpauta.usecase.IniciarVotacao;
 import io.swagger.annotations.Api;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -26,15 +24,10 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class PautaWS {
 
     private final CadastrarPauta cadastrarPauta;
-    private final IniciarVotacao iniciarVotacao;
 
     @PostMapping("/criar-pauta")
     public ResponseEntity<?> calculoValorFinanciado(@Valid @RequestBody PautaRequest pautaRequest) {
         return new ResponseEntity<>(cadastrarPauta.executar(pautaRequest.getPautaDTO()), HttpStatus.OK);
     }
 
-    @PostMapping("/iniciar-votacao}")
-    public ResponseEntity<?> calculoValorFinanciado(@Valid @RequestBody SessaoVotacaoRequest sessaoVotacaoRequest) {
-        return new ResponseEntity<>(iniciarVotacao.executar(sessaoVotacaoRequest), HttpStatus.OK);
-    }
 }
