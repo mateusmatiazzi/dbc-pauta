9e96bf1fe730b8cda498d67807cc75c3dc6bfb1a
mateusmatiazzi <mateuspaivamatiazzi1@gmail.com>
2021-10-24 19:40:42 -0300

    Configuração para trocar porta do swagger

--- src/main/java/br/com/dbc/pauta/dbcpauta/config/SwaggerConfig.java
+++ src/main/java/br/com/dbc/pauta/dbcpauta/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package br.com.dbc.pauta.dbcpauta.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -10,11 +12,19 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    @Value("${server.host:localhost}")
+    private String swaggerHost;
+
+    @Value("${server.port:8080}")
+    private String swaggerPort;
 
     @Bean
     public Docket config() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .host(swaggerHost + ":" + swaggerPort)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.dbc.pauta"))
                 .paths(PathSelectors.any())
